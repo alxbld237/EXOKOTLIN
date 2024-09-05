3f4f512602c6220d585d22defbee7ac89d5be885
Blondel <alxbld05@gmail.com>
2024-09-05 17:21:21 +0200

    EXO 5/09

--- 04-Les_variables/P04_d-Echange/src/main/kotlin/main.kt
+++ 04-Les_variables/P04_d-Echange/src/main/kotlin/main.kt
@@ -5,7 +5,9 @@ fun main() {
     var (a, b) = readLine()!!.split(" ").map { it.toInt() }
 
     // Write only exchange actions here. Do not touch the lines above
-    val c =
+    val c = a
+    a = b
+    b = c
 
 
         // Do not touch the lines below
--- 10-Les_types/10_1-Types_des_donnees/P10_1_b-Penser_compilateur/src/main/kotlin/main.kt
+++ 10-Les_types/10_1-Types_des_donnees/P10_1_b-Penser_compilateur/src/main/kotlin/main.kt
@@ -1,7 +1,9 @@
 
 
 fun main() {
-
+    val first = "alex"
+    val last = "denden"
+    val age = 25
     // put your code above
     println("My name is $first $last and I’m $age years old")
 }
