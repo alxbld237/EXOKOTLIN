75fa53b677f17bd58f2627db646fb45901e073e4
Blondel <alxbld05@gmail.com>
2024-09-17 11:56:18 +0200

    17/09

--- 17-Programmation_procedurale/17_2-Declaration_fonction/P17_2_a-Somme_3_nombres/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_2-Declaration_fonction/P17_2_a-Somme_3_nombres/src/main/kotlin/main.kt
@@ -1,10 +1,14 @@
-// write your function here
-
-fun main() {
-    val a = readLine()!!.toInt()
-    val b = readLine()!!.toInt()
-    val c = readLine()!!.toInt()
-
-    println(sum(a, b, c))
+fun sum (a: Int, b:Int, c:Int): Int {
+    val result = a+b+c
+    return result
 }
+
+    fun main() {
+        val a = readLine()!!.toInt()
+        val b = readLine()!!.toInt()
+        val c = readLine()!!.toInt()
+
+
+        println(sum(a, b, c))
+    }
 
--- 17-Programmation_procedurale/17_3-Arguments_defaut/P17_3_c-Controle_vitesse/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_3-Arguments_defaut/P17_3_c-Controle_vitesse/src/main/kotlin/main.kt
@@ -1,5 +1,11 @@
-fun checkSpeed(...) {
-    // write your code here
+fun checkSpeed(speed: Int, limit : Int=60) {
+   val voiture : Int
+   if(speed >limit){
+       voiture = speed -limit
+       return println("exces de vittesse de $voiture de kilometre par heure")
+   }else{
+       return println("dans les limit")
+   }
 }
 
 fun main() {
