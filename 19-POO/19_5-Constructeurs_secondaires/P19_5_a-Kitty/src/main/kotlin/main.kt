8f1dfcacfbd43d7a6a35b5293967a1c76d195678
Blondel <alxbld05@gmail.com>
2024-09-24 11:29:26 +0200

    24/09

--- 19-POO/19_5-Constructeurs_secondaires/P19_5_a-Kitty/src/main/kotlin/main.kt
+++ 19-POO/19_5-Constructeurs_secondaires/P19_5_a-Kitty/src/main/kotlin/main.kt
@@ -1,5 +1,29 @@
 class Kitty {
-    // write here
+    var color: String = ""
+    var age: Int = 0
+
+    constructor(color: String, age: Int) {
+        this.color = color
+        this.age = age
+    }
+
+    constructor(age: Int, color: String) {
+        this.age = age
+        this.color = color
+    }
+
+    constructor(color: String) {
+        this.color = color
+
+        this.age = 0
+    }
+
+
+    constructor(age: Int) {
+        this.age = age
+
+        this.color = "Unknown"
+    }
 }
 
 
